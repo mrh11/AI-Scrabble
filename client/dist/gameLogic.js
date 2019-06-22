26c877a9ed8fa1c051e5099056177e621e13dbf1
Raihan <raihanhossain0@gmail.com>
2019-06-22 11:49:49 -0400

    add more game logic

--- client/dist/gameLogic.js
+++ client/dist/gameLogic.js
@@ -1,3 +1,11 @@
+let scrabbleBag = [
+  "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "A", "I", "A", "I", "A", "I", "A", "I",
+ "A", "I", "A", "I", "A", "I", "A", "I", "A", "I", "O", "O", "O", "O", "O", "O", "O", "O", "N", "R",
+ "T", "N", "R", "T", "N", "R", "T", "N", "R", "T", "N", "R", "T", "N", "R", "T", "L", "S", "U", "D",
+ "L", "S", "U", "D", "L", "S", "U", "D", "L", "S", "U", "D", "G", "G", "G", "B", "C", "M", "P", "F",
+ "H", "V", "W", "Y", "B", "C", "M", "P", "F", "H", "V", "W", "Y", "K", "J", "X", "Q", "Z", "BLANK", "BLANK"
+];
+
 const letterPoints = {
   'BLANK': 0,
   'A': 1,  'E': 1,  'I': 1,  'L': 1,  'N': 1,  'R': 1,  'S': 1,  'T': 1,  'U': 1,
@@ -8,18 +16,19 @@ const letterPoints = {
   'J': 8,  'X': 8,
   'Q': 10, 'Z': 10
 }
-//may not need this code here:
-const letterFrequency = {
-  'K': 1,  'J': 1,  'X': 1,  'Q': 1,  'Z': 1,
-  'BLANK': 2,  'B': 2,  'C': 2,  'M': 2,  'P': 2,  'F': 2,  'M': 2,  'V': 2,  'W': 2,  'Y': 2,
-  'G': 3,
-  'L': 4,  'S': 4,  'U': 4,  'D': 4,
-  'N': 6,  'R': 6,  'T': 6,
-  'O': 8,
-  'A': 9,  'I': 9,
-  'E': 12
+
+const shuffleScrabbleBag = (arr) => {
+  //fischer-yates algorithm
+  for (let i = 99; i > 0; i--) {
+    let j = Math.floor(Math.random()*(i+1));
+    let temp = arr[i];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
 }
 
+//needs to modified to account for word and letter multipliers
 const wordScore = (str) => {
   let score = 0;
   for (let i = 0; i < str.length; i++) {
@@ -54,3 +63,19 @@ Board.prototype.create_board = (size) => {
   return m;
 }
 
+const preventBoardOverwrite = (board, x_pos, y_pos, letter) => {
+  if (board[y_pos][x_pos] === '') board[y_pos][x_pos] === letter;
+  else console.log('Invalid Move');
+}
+
+//may not need this code here:
+// const letterFrequency = {
+//   'K': 1,  'J': 1,  'X': 1,  'Q': 1,  'Z': 1,
+//   'BLANK': 2,  'B': 2,  'C': 2,  'M': 2,  'P': 2,  'F': 2,  'H': 2,  'V': 2,  'W': 2,  'Y': 2,
+//   'G': 3,
+//   'L': 4,  'S': 4,  'U': 4,  'D': 4,
+//   'N': 6,  'R': 6,  'T': 6,
+//   'O': 8,
+//   'A': 9,  'I': 9,
+//   'E': 12
+// }
---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from 'react';
+import ReactDom from 'reactdom';
+import Axios from 'axios';
+
+
+const App = () => {
+  const [player, switchPlayer] = useState(1);
+}
